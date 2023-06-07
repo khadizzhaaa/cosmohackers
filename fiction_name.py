356a0ba7fbf61c0ce7e56054a0091af89243a636
vladadpetrukhina20 <petrukhinavlada@gmail.com>
2023-06-07 13:13:30 +0300

    исправила ошибки

--- fiction_name.py
+++ fiction_name.py
@@ -1,4 +1,6 @@
 def get_pseudonym(name_surname):
+    FIRST_NAME = {'A': 'Alpha', 'B': 'Beta', 'C': 'Cache', 'D': 'Data', 'E': 'Energy', 'F': 'Function', 'G': 'Gloom', 'H': 'Holo', 'I': 'Inovation', 'J': 'Jealous', 'K': 'Katana', 'L': 'Logic', 'M': 'Malware', 'N': 'Negative', 'O': 'Operative', 'P': 'Paragon', 'Q': 'Quant', 'R': 'Recursive', 'S': 'Seismic', 'T': 'Triangle', 'U': 'Ultra', 'V': 'Violent', 'W': 'WiFi', 'X': 'X-ray', 'Y': 'Yell', 'Z': 'Zephyr'}
+    SURNAME = {'A': 'Analogue', 'B': 'Bomb', 'C': 'Catalyst', 'E': 'Enormous', 'F': 'Field', 'G': 'Garbage', 'H': 'Half-life', 'I': 'Intranet', 'J': 'Jack', 'K': 'Killer', 'L': 'Lava', 'M': 'Madness', 'N': 'Nickname', 'O': 'Ops', 'P': 'Payload', 'Q': 'Qubit', 'R': 'Radiant', 'S': 'Sygnus', 'T': 'T-Rex', 'U': 'Unity', 'V': 'Vox','W': 'Worm','X': 'Xeon','Y': 'Yawn','Z': 'Zeppelin'}
     name = name_surname.split(' ')[0]
     surname = name_surname.split(' ')[1]
     name = name.upper()
@@ -10,6 +12,9 @@ def get_pseudonym(name_surname):
         return psev_surname + ' ' + psev_name
     else:
         return "Ваше имя должно начинаться с буквы от A до Z"
+        
+print(get_pseudonym(name_surname))
+    
     
 # У меня на экране при запуске этой задачи нет никакого вывода.
 # Почему?
